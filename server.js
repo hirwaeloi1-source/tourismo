a401b282da2f035631a74b219c9ed5153ad93985
Hirwa Eloi <hirwaeloi1@gmail.com>
2026-07-22 13:17:57 +0200

    Fix SMTP configuration

--- server.js
+++ server.js
@@ -176,17 +176,14 @@ const User = mongoose.model("User", userSchema);
 // ======================================
 
 const transporter = nodemailer.createTransport({
-
-    service: "gmail",
-
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
-
         user: process.env.EMAIL_USER,
-
         pass: process.env.EMAIL_PASS
-
-    }
-
+    },
+    connectionTimeout: 30000
 });
 
 
